1eaa68acbfd536dfd4dddfcae8e050b4765071f7
yon30c <yoncst30@gmail.com>
2023-07-21 22:10:19 -0400

    store

--- lib/src/featured/screens/product/product_screen.dart
+++ lib/src/featured/screens/product/product_screen.dart
@@ -1,7 +1,8 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:store_template/src/shared/models/product.dart';
 
-class ProductScreen extends StatelessWidget {
+class ProductScreen extends StatefulWidget {
   static const String name = 'product-screen';
 
   const ProductScreen({super.key, required this.product});
@@ -9,14 +10,70 @@ class ProductScreen extends StatelessWidget {
   final Product product;
 
   @override
+  State<ProductScreen> createState() => _ProductScreenState();
+}
+
+class _ProductScreenState extends State<ProductScreen> {
+  late SwiperController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = SwiperController();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final color = Theme.of(context).colorScheme;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ProductScreen'),
-      ),
-      body: const Center(
-        child: Text('ProductScreen'),
-      ),
+      backgroundColor: color.primaryContainer,
+        appBar: AppBar(
+          title: Text(widget.product.title),
+        ),
+        body: Column(
+          children: [
+            _CsImages(size: size, controller: controller, widget: widget),
+          ],
+        ));
+  }
+}
+
+class _CsImages extends StatelessWidget {
+  const _CsImages({
+    super.key,
+    required this.size,
+    required this.controller,
+    required this.widget,
+  });
+
+  final Size size;
+  final SwiperController controller;
+  final ProductScreen widget;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: size.height * 0.4,
+      child: Swiper(
+        controller: controller,
+
+          outer: true,
+          pagination: const SwiperPagination(),
+          viewportFraction: 0.9,
+          itemCount: 3,
+          itemBuilder: (context, index) => Container(
+            decoration:  BoxDecoration(
+              borderRadius: BorderRadius.circular(20)
+            ),
+            clipBehavior: Clip.antiAlias,
+            margin: const EdgeInsets.all(8.0),
+            child: Image.asset(
+                  widget.product.imageUrl,
+                  fit: BoxFit.cover,
+                ),
+          )),
     );
   }
 }
